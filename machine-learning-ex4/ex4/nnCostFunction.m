ab0d1760e721a41c2d262e69efcee9250e982062
Koeh, Johann <jkoeh@illumina.com>
2018-01-12 09:34:56 -0800

    cost function unregularized

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -24,12 +24,20 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
+
          
 % You need to return the following variables correctly 
 J = 0;
+X = [ones(m,1), X];
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
-
+a2 = sigmoid(X*Theta1');
+a2 = [ones(m,1), a2];
+a3 = sigmoid(a2*Theta2');
+eye_matrix = eye(num_labels);
+y_matrix = eye_matrix(y,:);
+    
+J = (1/m *sum(sum(-y_matrix .* log(a3) -(1-y_matrix).*log(1-a3))));
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
 %               following parts.
